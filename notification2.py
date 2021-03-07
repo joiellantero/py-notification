d48fe69e775dec662d2cea65d4901c99e7b16059
joiellantero <jtllantero@up.edu.ph>
2021-03-08 00:24:58 +0800

    Create notification2.py

---
+++ notification2.py
@@ -0,0 +1,24 @@
+import os
+import smtplib 
+from email.message import EmailMessage
+from dotenv import load_dotenv
+load_dotenv()
+
+EMAIL_CLIENT = os.getenv('EMAIL_CLIENT')
+EMAIL_CLIENT_APP_PASSWORD = os.getenv('EMAIL_CLIENT_APP_PASSWORD')
+
+def email_alert(subject, to, body):
+    message = EmailMessage()
+    message['subject'] = subject
+    message['to'] = to
+    message['from'] = EMAIL_CLIENT
+    message.set_content(body)
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(EMAIL_CLIENT, EMAIL_CLIENT_APP_PASSWORD)
+    server.send_message(message)
+    server.quit()
+
+if __name__ == '__main__':
+    email_alert("this is my subject", "recipient@email.com", "this is my body")
